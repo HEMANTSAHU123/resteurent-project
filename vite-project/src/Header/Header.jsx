c2e95fb6ae9b95e4be44342790d65781f33a2829
HEMANTSAHU123 <hs1282542@gmail.com>
2025-02-16 16:50:41 +0530

    first change

---
+++ vite-project/src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div>
+      <header className='header'>
+        <h1>ReactMeals</h1>
+        <div className='card'><span>Your cart</span></div>
+      </header>
+
+    </div>
+  )
+}
+
+export default Header
