facd9081717d82b81400e4a6b337f5a0939697a1
HEMANTSAHU123 <hs1282542@gmail.com>
2025-02-16 18:09:26 +0530

    second changes

--- vite-project/src/Header/Header.jsx
+++ vite-project/src/Header/Header.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import "./Header.css"
 const Header = () => {
   return (
-    <div>
+    <div className='main'>
       <header className='header'>
         <h1>ReactMeals</h1>
-        <div className='card'><span>Your cart</span></div>
+        <button className='button'>Your cart <span>o</span></button>
       </header>
-
+      <div className="header-contents">
+        <h1>Delicious Food, delivered to you</h1>
+     
+       <p>Choose your favourite meal from your broad selection of available meal and 
+        joy out delicioud food 
+          lunch at home </p>
+          <p>All our meal are cooked with highly infradient
+          ,just in time and ofcourse by experience chefs</p>
+   </div>
     </div>
   )
 }
